3e1715b7a409112f914d8828939e59e0dfe87fa6
Tim Kettering <tim@blackcore.com>
2015-07-28 14:50:32 -0700

    adding dummy kinect2 server

---
+++ dummy-kinect2-server/index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(3000);
+var json = require('json-file');
+var _ = require('lodash');
+var sleep = require('sleep');
+
+var output = json.read('./skeletal-output-02.json');
+var array = output.get('bodiesData')
+
+io.on('connection', function(socket){
+	console.log("Received connection.");
+	var counter = 0;
+	var arraySize = array.length;
+	
+	setInterval(function() {
+		socket.emit("bodyFrame", array[counter]);
+		
+		counter++;
+		if(counter >= array.length) {
+			counter = 0;
+		}		
+	}, 33);	
+});
