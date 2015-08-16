9ecbcdc28e1d4c77ffc2c18f67d72e2950007cc3
James Tranovich <jtranovich@gmail.com>
2015-08-15 17:31:39 -0700

    Change proper paths.

--- startup/startup-kinect.js
+++ startup/startup-kinect.js
@@ -3,7 +3,7 @@ var Service = require('node-windows').Service;
 var svc = new Service({
   name: 'SoS Kinect Server',
   description: 'Sanctuary of Self Kinect Server.',
-  script: 'C:\\Users\\James\\Desktop\\sos-display\\kinect2-server\\index.js'
+  script: 'C:\\Users\\kinectdev\\Desktop\\sos-display\\kinect2-server\\index.js'
 });
 
 svc.on('install',function(){
