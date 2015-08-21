ca443b8ea29f43fe2caccc6a4a04ac3725c01a73
Jon Plaut <jon@brightbytes.net>
2015-08-21 17:12:52 -0400

    small refactor of audio.js

--- app/modes/disco/audio.js
+++ app/modes/disco/audio.js
@@ -9,26 +9,29 @@ mode.factory('audioDisco', function($rootScope, audioService, $log) {
   var _step = (1 - _baseVolume) / _maxBodies;
   var _gainNode = audioService.context.createGain();
   var _bufferSource = audioService.context.createBufferSource();
+  
+  var mode = {};
 
-  return {
-    start: function() {
-      audioService.loadBuffer('disco.wav').then(function(buffer) {
-        _bufferSource.buffer = buffer;
-        _bufferSource.loop = true;
-        _gainNode.gain.value = _baseVolume;
-        _bufferSource.connect(_gainNode);
-        _gainNode.connect(audioService.context.destination);
-        _bufferSource.start(0);
+  mode.start = function() {
+    audioService.loadBuffer('disco.wav').then(function(buffer) {
+      _bufferSource.buffer = buffer;
+      _bufferSource.loop = true;
+      _gainNode.gain.value = _baseVolume;
+      _bufferSource.connect(_gainNode);
+      _gainNode.connect(audioService.context.destination);
+      _bufferSource.start(0);
 
-        _cancelListener = $rootScope.$on('kinectBodiesUpdate', function(e, bodies) {
-          var newValue = _baseVolume + Object.keys(bodies).length * _step;
-          _gainNode.gain.value = newValue;
-        });
+      _cancelListener = $rootScope.$on('kinectBodiesUpdate', function(e, bodies) {
+        var newValue = _baseVolume + Object.keys(bodies).length * _step;
+        _gainNode.gain.value = newValue;
       });
-    },
-    stop: function() {
-      _cancelListener();
-      _bufferSource.stop();
-    }
+    });
   }
+
+  mode.stop = function() {
+    _cancelListener();
+    _bufferSource.stop();
+  }
+
+  return mode;
 });
